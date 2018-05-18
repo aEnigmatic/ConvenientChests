40eaa948b36d53a80a70bb6575411d5aa42fee19
aEnigmatic <aenigmatic@users.noreply.github.com>
2018-05-18 17:43:58 +0200

    Fixed Fridge not being found for stash to nearby

--- ConvenientChests/StackToNearbyChests/StackLogic.cs
+++ ConvenientChests/StackToNearbyChests/StackLogic.cs
@@ -7,36 +7,32 @@ using StardewValley;
 using StardewValley.Buildings;
 using StardewValley.Locations;
 using StardewValley.Objects;
+using Object = StardewValley.Object;
 
-namespace ConvenientChests.StackToNearbyChests
-{
-    public static class StackLogic
-    {
-        public static IEnumerable<Chest> getNearbyChests(this Farmer farmer, int radius)
-            => getNearbyChests(farmer.currentLocation, farmer.getTileLocation(), radius);
+namespace ConvenientChests.StackToNearbyChests {
+    public static class StackLogic {
+        public static IEnumerable<Chest> GetNearbyChests(this Farmer farmer, int radius)
+            => GetNearbyChests(farmer.currentLocation, farmer.getTileLocation(), radius);
 
-        public static void StashToNearbyChests(int radius, Func<Chest, Item, bool> acceptingFunction)
-        {
+        public static void StashToNearbyChests(int radius, Func<Chest, Item, bool> acceptingFunction) {
             if (Game1.player.currentLocation == null)
                 return;
 
             var farmer = Game1.player;
             var items = farmer.Items
-                .Where(i => i != null)
-                .ToList();
+                              .Where(i => i != null)
+                              .ToList();
 
             ModEntry.staticMonitor.Log("Stash to nearby");
 //            ChestMod.staticMonitor.Log($"Trying to move: " + string.Join(", ", items.Select(i => i.Name)));
 
-            
             var movedAtLeastOne = false;
 
-            foreach (var chest in farmer.getNearbyChests(radius))
-            {
+            foreach (var chest in farmer.GetNearbyChests(radius)) {
 //                ChestMod.staticMonitor.Log($"\tFound chest at {chest.TileLocation}");
                 var moveItems = items
-                    .Where(i => acceptingFunction(chest, i))
-                    .ToList();
+                               .Where(i => acceptingFunction(chest, i))
+                               .ToList();
 
                 if (!moveItems.Any())
                     continue;
@@ -54,32 +50,37 @@ namespace ConvenientChests.StackToNearbyChests
             Game1.playSound(Game1.soundBank.GetCue("pickUpItem").Name);
         }
 
-        private static IEnumerable<Chest> getNearbyChests(GameLocation location, Vector2 point, int radius)
-        {
+        private static IEnumerable<Chest> GetNearbyChests(GameLocation location, Vector2 point, int radius) {
             // chests
-            foreach (var p in location.Objects.Pairs)
-            {
-                if (!(p.Value is Chest c))
-                    continue;
+            foreach (Chest c in GetNearbyObjects<Chest>(location, point, radius))
+                yield return c;
 
-                // c.TileLocation = p.Key * Game1.tileSize;
-                if (inRadius(radius, point, p.Key))
-                    yield return c;
+
+            switch (location) {
+                // fridge
+                case FarmHouse farmHouse when farmHouse.upgradeLevel > 0:
+                    if (InRadius(radius, point, farmHouse.getKitchenStandingSpot().X, farmHouse.getKitchenStandingSpot().Y - 2))
+                        yield return farmHouse.fridge.Value;
+                    break;
+
+                // buildings
+                case BuildableGameLocation l:
+                    foreach (var building in l.buildings.Where(b => InRadius(radius, point, b.tileX.Value, b.tileY.Value)))
+                        if (building is JunimoHut junimoHut)
+                            yield return junimoHut.output.Value;
+
+                        else if (building is Mill mill)
+                            yield return mill.output.Value;
+                    break;
             }
-
-            // buildings
-            if (!(location is BuildableGameLocation l))
-                yield break;
-
-            foreach (var building in l.buildings.Where(b => inRadius(radius, point, b.tileX.Value, b.tileY.Value)))
-                if (building is JunimoHut junimoHut)
-                    yield return junimoHut.output.Value;
-
-                else if (building is Mill mill)
-                    yield return mill.output.Value;
         }
 
-        private static bool inRadius(int radius, Vector2 a, Vector2 b) => Math.Abs(a.X - b.X) < radius && Math.Abs(a.Y - b.Y) < radius;
-        private static bool inRadius(int radius, Vector2 a, int x, int y) => Math.Abs(a.X - x) < radius && Math.Abs(a.Y - y) < radius;
+        private static IEnumerable<T> GetNearbyObjects<T>(GameLocation location, Vector2 point, int radius) where T : Object =>
+            location.Objects.Pairs
+                    .Where(p => p.Value is T && InRadius(radius, point, p.Key))
+                    .Select(p => (T) p.Value);
+
+        private static bool InRadius(int radius, Vector2 a, Vector2 b)        => Math.Abs(a.X - b.X) < radius && Math.Abs(a.Y - b.Y) < radius;
+        private static bool InRadius(int radius, Vector2 a, int     x, int y) => Math.Abs(a.X - x)   < radius && Math.Abs(a.Y - y)   < radius;
     }
 }
