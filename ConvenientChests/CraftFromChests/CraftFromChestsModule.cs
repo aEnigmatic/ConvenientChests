980c9520f02a8d1fafb5fc5285d4d26df411650e
Guido Kubach <guido.kubach@media-it-support.de>
2018-05-20 19:10:34 +0200

    Fixed Crafting from cooking screen not working

--- ConvenientChests/CraftFromChests/CraftFromChestsModule.cs
+++ ConvenientChests/CraftFromChests/CraftFromChestsModule.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using ConvenientChests.CategorizeChests;
@@ -19,16 +19,17 @@ namespace ConvenientChests.CraftFromChests {
         private IList<IList<Item>> CraftInventories        { get; set; }
         private List<List<Item>>   CraftInventoriesInitial { get; set; }
 
-        private        bool IsCraftingScreen => GameMenu?.currentTab == GameMenu.craftingTab;
-        private        bool IsCookingScreen  => IsCraftingScreen && GameMenu.GetType() == Type.GetType("CookingSkill.NewCraftingPage, CookingSkill");
-        private static bool HasFridge        => Utility.getHomeOfFarmer(Game1.player).upgradeLevel > 0;
+        private static bool IsCraftingScreen => Game1.activeClickableMenu is GameMenu m && m.currentTab == GameMenu.craftingTab;
+        private static bool IsCookingScreen  => Game1.activeClickableMenu is StardewValley.Menus.CraftingPage;
+        private static bool HasFridge        => Utility.getHomeOfFarmer(Game1.MasterPlayer).upgradeLevel > 0;
 
         public CraftFromChestsModule(ModEntry modEntry) : base(modEntry) { }
 
         public override void activate() {
-            GameMenuExtension.Shown      += OnGameMenuShown;
-            MenuEvents.MenuClosed        += OnMenuClosed;
-            GameMenuExtension.TabChanged += OnGameMenuTabChanged;
+            MenuListener.RegisterEvents();
+
+            MenuListener.CraftingMenuShown  += (sender, e) => Hijack();
+            MenuListener.CraftingMenuClosed += (sender, e) => Restore();
         }
 
         private void Hijack() {
@@ -38,10 +39,7 @@ namespace ConvenientChests.CraftFromChests {
 
             try {
                 CombineInventories(nearbyChests);
-
-                var tabs = GameMenu.getTabs();
-                var page = (StardewValley.Menus.CraftingPage) tabs[GameMenu.craftingTab];
-                page.inventory.actualInventory = UserInventory;
+                ReplaceCraftingInventory();
 
                 GameEvents.HalfSecondTick += UpdateEvent;
             }
@@ -49,6 +47,26 @@ namespace ConvenientChests.CraftFromChests {
                 Monitor.Log("Something went wrong! Consider disabling CraftFromChests:", LogLevel.Alert);
                 Monitor.Log(e.ToString(),                                                LogLevel.Error);
                 Restore();
+            }
+        }
+
+        private void ReplaceCraftingInventory() {
+            switch (Game1.activeClickableMenu) {
+                case GameMenu gameMenu:
+                    var tabs = gameMenu.GetTabs(modEntry.Helper.Reflection);
+                    var page = (StardewValley.Menus.CraftingPage) tabs[GameMenu.craftingTab];
+                    page.inventory.showGrayedOutSlots = true;
+                    page.inventory.actualInventory    = UserInventory;
+                    break;
+
+                case StardewValley.Menus.CraftingPage p:
+                    p.inventory.showGrayedOutSlots = false;
+                    p.inventory.actualInventory    = UserInventory;
+                    break;
+
+                default:
+                    // How did we get here?
+                    throw new Exception($"Unexpected menu: {Game1.activeClickableMenu?.GetType().ToString() ?? "null"}");
             }
         }
 
@@ -164,7 +182,7 @@ namespace ConvenientChests.CraftFromChests {
                 }
         }
 
-        private void CombineInventories(IEnumerable<Chest> chests) {
+        private void CombineInventories(ICollection<Chest> chests) {
             if (UserInventory != null)
                 return;
 
@@ -184,37 +202,19 @@ namespace ConvenientChests.CraftFromChests {
                 Game1.player.Items.Add(null);
         }
 
-        private IEnumerable<IList<Item>> GetInventories(IEnumerable<Chest> chests) {
+        private IEnumerable<IList<Item>> GetInventories(ICollection<Chest> chests) {
             yield return UserInventory;
 
             foreach (var c in chests)
                 yield return c.items;
 
             // always add fridge when on cooking screen
-            if (IsCookingScreen && HasFridge)
-                yield return Utility.getHomeOfFarmer(Game1.player).fridge.Value.items;
-        }
+            if (!IsCookingScreen || !HasFridge)
+                yield break;
 
-        private void OnGameMenuShown(object sender, EventArgs e) {
-            GameMenu = Game1.activeClickableMenu as GameMenu;
-
-            if (IsCraftingScreen)
-                Hijack();
-        }
-
-        private void OnGameMenuTabChanged(object sender, EventArgs e) {
-            if (IsCraftingScreen)
-                Hijack();
-
-            else
-                Restore();
-        }
-
-        private void OnMenuClosed(object sender, EventArgsClickableMenuClosed e) {
-            if (GameMenu == null)
-                return;
-
-            Restore();
+            Chest fridge = Utility.getHomeOfFarmer(Game1.MasterPlayer).fridge.Value;
+            if (!chests.Contains(fridge))
+                yield return fridge.items;
         }
 
         private void UpdateEvent(object sender, EventArgs e) => CleanupInventories();
