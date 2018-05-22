13a996bea2cf3047e8678dba0b7bb53f90e560c2
aEnigmatic <aenigmatic@users.noreply.github.com>
2018-05-22 18:09:49 +0200

    Added Furniture to blacklist

--- ConvenientChests/CategorizeChests/Framework/ItemBlacklist.cs
+++ ConvenientChests/CategorizeChests/Framework/ItemBlacklist.cs
@@ -13,7 +13,8 @@ namespace ConvenientChests.CategorizeChests.Framework
         /// </summary>
         /// <returns>Whether the key is blacklisted.</returns>
         /// <param name="itemKey">Item key to check.</param>
-        public static bool Includes(ItemKey itemKey) => itemKey.ItemType == ItemType.BigCraftable || BlacklistedItemKeys.Contains(itemKey);
+        public static bool Includes(ItemKey itemKey) => 
+            itemKey.ItemType == ItemType.BigCraftable ||itemKey.ItemType == ItemType.Furniture || BlacklistedItemKeys.Contains(itemKey);
 
         private static readonly HashSet<ItemKey> BlacklistedItemKeys = new HashSet<ItemKey> {
             // stones
