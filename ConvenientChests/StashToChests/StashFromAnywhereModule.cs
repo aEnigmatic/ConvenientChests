bf90324c9b108b7129fea00398eed8562591a346
aEnigmatic <aenigmatic@users.noreply.github.com>
2020-02-11 12:52:58 +0100

    Fix stashing to fridge

--- ConvenientChests/StashToChests/StashFromAnywhereModule.cs
+++ ConvenientChests/StashToChests/StashFromAnywhereModule.cs
@@ -5,84 +5,66 @@ using StardewValley;
 using StardewValley.Objects;
 using Utility = ConvenientChests.CategorizeChests.Utility;
 
-namespace ConvenientChests.StashToChests
-{
-    public class StashFromAnywhereModule : Module
-    {
-        private StackLogic.AcceptingFunction CategorizedAcceptingFunction { get; set; }
+namespace ConvenientChests.StashToChests {
+    public class StashFromAnywhereModule : Module {
+        private StackLogic.AcceptingFunction CategorizedAcceptingFunction    { get; set; }
         private StackLogic.AcceptingFunction ExistingStacksAcceptingFunction { get; set; }
 
-        public StashFromAnywhereModule(ModEntry modEntry) : base(modEntry)
-        {
-        }
+        public StashFromAnywhereModule(ModEntry modEntry) : base(modEntry) { }
 
-        private StackLogic.AcceptingFunction CreateCategorizedAcceptingFunction()
-        {
-            if (Config.StashAnywhere)
-            {
+        private StackLogic.AcceptingFunction CreateCategorizedAcceptingFunction() {
+            if (Config.StashAnywhere) {
                 return (chest, item) => ModEntry.CategorizeChests.ChestAcceptsItem(chest, item);
             }
 
             return (chest, item) => false;
         }
 
-        private StackLogic.AcceptingFunction CreateExistingStacksAcceptingFunction()
-        {
-            if (Config.StashAnywhere && Config.StashAnywhereToExistingStacks)
-            {
+        private StackLogic.AcceptingFunction CreateExistingStacksAcceptingFunction() {
+            if (Config.StashAnywhere && Config.StashAnywhereToExistingStacks) {
                 return (chest, item) => chest.ContainsItem(item);
             }
-            
+
             return (chest, item) => false;
         }
 
-        public override void Activate()
-        {
+        public override void Activate() {
             IsActive = true;
 
-            CategorizedAcceptingFunction = CreateCategorizedAcceptingFunction();
+            CategorizedAcceptingFunction    = CreateCategorizedAcceptingFunction();
             ExistingStacksAcceptingFunction = CreateExistingStacksAcceptingFunction();
 
             this.Events.Input.ButtonPressed += OnButtonPressed;
         }
 
-        public override void Deactivate()
-        {
-            IsActive = false;
+        public override void Deactivate() {
+            IsActive                        =  false;
             this.Events.Input.ButtonPressed -= OnButtonPressed;
         }
 
-        private void StashGlobally()
-        {
+        private void StashGlobally() {
             // try to stash to fridge first
-            if (Config.StashAnywhereToFridge && ChestExtension.GetFridge(Game1.player) is Chest fridge)
+            if (Config.StashAnywhereToFridge && ChestExtension.GetFridge(Game1.player) is Chest fridge) {
                 StackLogic.StashToChest(fridge, CategorizedAcceptingFunction);
+                StackLogic.StashToChest(fridge, ExistingStacksAcceptingFunction);
+            }
 
             // try to find all chests by location
             if (Game1.player.currentLocation == null)
                 return;
 
-            var locations = Utility.GetLocations().ToList();
-            
-            foreach (var pair in locations.SelectMany(location => location.Objects.Pairs))
-            {
-                if (pair.Value is Chest chest)
-                {
-                    StackLogic.StashToChest(chest, CategorizedAcceptingFunction);
-                }
-            }
-            
-            foreach (var pair in locations.SelectMany(location => location.Objects.Pairs))
-            {
-                if (pair.Value is Chest chest)
-                {
-                    StackLogic.StashToChest(chest, ExistingStacksAcceptingFunction);
-                }
-            }
+            var chests = Utility.GetLocations().SelectMany(location => location.Objects.Pairs).Select(pair => pair.Value).OfType<Chest>().ToList();
+
+            // stash by category
+            foreach (var chest in chests)
+                StackLogic.StashToChest(chest, CategorizedAcceptingFunction);
+
+            // stash by existing
+            foreach (var chest in chests)
+                StackLogic.StashToChest(chest, ExistingStacksAcceptingFunction);
         }
 
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
-        {
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e) {
             if (e.Button == Config.StashAnywhereKey)
                 StashGlobally();
         }
