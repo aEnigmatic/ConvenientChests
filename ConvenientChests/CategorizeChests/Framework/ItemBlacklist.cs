fda54c0671e08b423ce016de9e38b715bdad69ff
aschismatism <164274962+aschismatism@users.noreply.github.com>
2024-03-23 17:01:07 -0700

    Filter out a lot of unobtainable items.

--- ConvenientChests/CategorizeChests/Framework/ItemBlacklist.cs
+++ ConvenientChests/CategorizeChests/Framework/ItemBlacklist.cs
@@ -20,9 +20,30 @@ namespace ConvenientChests.CategorizeChests.Framework
             // stones
             new ItemKey(ItemType.Object, "2"),
             new ItemKey(ItemType.Object, "4"),
+            new ItemKey(ItemType.Object, "6"),
+            new ItemKey(ItemType.Object, "8"),
+            new ItemKey(ItemType.Object, "10"),
+            new ItemKey(ItemType.Object, "12"),
+            new ItemKey(ItemType.Object, "14"),
+            new ItemKey(ItemType.Object, "25"),
+            new ItemKey(ItemType.Object, "32"),
+            new ItemKey(ItemType.Object, "34"),
+            new ItemKey(ItemType.Object, "36"),
+            new ItemKey(ItemType.Object, "38"),
+            new ItemKey(ItemType.Object, "40"),
+            new ItemKey(ItemType.Object, "42"),
+            new ItemKey(ItemType.Object, "44"),
+            new ItemKey(ItemType.Object, "46"),
+            new ItemKey(ItemType.Object, "48"),
+            new ItemKey(ItemType.Object, "50"),
+            new ItemKey(ItemType.Object, "52"),
+            new ItemKey(ItemType.Object, "54"),
+            new ItemKey(ItemType.Object, "56"),
+            new ItemKey(ItemType.Object, "58"),
             new ItemKey(ItemType.Object, "75"),
             new ItemKey(ItemType.Object, "76"),
             new ItemKey(ItemType.Object, "77"),
+            new ItemKey(ItemType.Object, "95"),
             new ItemKey(ItemType.Object, "290"),
             new ItemKey(ItemType.Object, "343"),
             new ItemKey(ItemType.Object, "450"),
@@ -33,6 +54,26 @@ namespace ConvenientChests.CategorizeChests.Framework
             new ItemKey(ItemType.Object, "762"),
             new ItemKey(ItemType.Object, "764"),
             new ItemKey(ItemType.Object, "765"),
+            new ItemKey(ItemType.Object, "816"),
+            new ItemKey(ItemType.Object, "817"),
+            new ItemKey(ItemType.Object, "818"),
+            new ItemKey(ItemType.Object, "819"),
+            new ItemKey(ItemType.Object, "843"),
+            new ItemKey(ItemType.Object, "844"),
+            new ItemKey(ItemType.Object, "845"),
+            new ItemKey(ItemType.Object, "846"),
+            new ItemKey(ItemType.Object, "847"),
+            new ItemKey(ItemType.Object, "849"),
+            new ItemKey(ItemType.Object, "850"),
+            new ItemKey(ItemType.Object, "BasicCoalNode0"),
+            new ItemKey(ItemType.Object, "BasicCoalNode1"),
+            new ItemKey(ItemType.Object, "CalicoEggStone_0"),
+            new ItemKey(ItemType.Object, "CalicoEggStone_1"),
+            new ItemKey(ItemType.Object, "CalicoEggStone_2"),
+            new ItemKey(ItemType.Object, "PotOfGold"),
+            new ItemKey(ItemType.Object, "VolcanoGoldNode"),
+            new ItemKey(ItemType.Object, "VolcanoCoalNode0"),
+            new ItemKey(ItemType.Object, "VolcanoCoalNode1"),
 
             // weeds
             new ItemKey(ItemType.Object, "0"),
@@ -59,11 +100,54 @@ namespace ConvenientChests.CategorizeChests.Framework
             new ItemKey(ItemType.Object, "792"),
             new ItemKey(ItemType.Object, "793"),
             new ItemKey(ItemType.Object, "794"),
+            new ItemKey(ItemType.Object, "882"),
+            new ItemKey(ItemType.Object, "883"),
+            new ItemKey(ItemType.Object, "884"),
+            new ItemKey(ItemType.Object, "GreenRainWeeds0"),
+            new ItemKey(ItemType.Object, "GreenRainWeeds1"),
+            new ItemKey(ItemType.Object, "GreenRainWeeds2"),
+            new ItemKey(ItemType.Object, "GreenRainWeeds3"),
+            new ItemKey(ItemType.Object, "GreenRainWeeds4"),
+            new ItemKey(ItemType.Object, "GreenRainWeeds5"),
+            new ItemKey(ItemType.Object, "GreenRainWeeds6"),
+            new ItemKey(ItemType.Object, "GreenRainWeeds7"),
 
             // twigs
             new ItemKey(ItemType.Object, "294"),
             new ItemKey(ItemType.Object, "295"),
 
+            // quest items
+            new ItemKey(ItemType.Object, "71"), // Trimmed Lucky Purple Shorts
+            new ItemKey(ItemType.Object, "191"), // Ornate Necklace
+            new ItemKey(ItemType.Object, "742"), // Haley's Lost Bracelet
+            new ItemKey(ItemType.Object, "788"), // Lost Axe
+            new ItemKey(ItemType.Object, "789"), // Lucky Purple Shorts
+            new ItemKey(ItemType.Object, "790"), // Berry Basket
+            new ItemKey(ItemType.Object, "864"), // War Memento
+            new ItemKey(ItemType.Object, "865"), // Gourmet Tomato Salt
+            new ItemKey(ItemType.Object, "866"), // Stardew Valley Rose
+            new ItemKey(ItemType.Object, "867"), // Advanced TV Remote
+            new ItemKey(ItemType.Object, "868"), // Arctic Shard
+            new ItemKey(ItemType.Object, "869"), // Wriggling Worm
+            new ItemKey(ItemType.Object, "870"), // Pirate's Locket
+            new ItemKey(ItemType.Object, "875"), // Ectoplasm
+            new ItemKey(ItemType.Object, "876"), // Prismatic Jelly
+            new ItemKey(ItemType.Object, "870"), // Pirate's Locket
+            new ItemKey(ItemType.Object, "897"), // Pierre's Missing Stocklist
+            new ItemKey(ItemType.Object, "GoldenBobber"),
+
+            // unstorable items (used on pickup)
+            new ItemKey(ItemType.Object, "73"), // Golden Walnut
+            new ItemKey(ItemType.Object, "434"), // Stardrop
+            new ItemKey(ItemType.Object, "858"), // Qi Gem
+
+            // supply crates
+            new ItemKey(ItemType.Object, "922"),
+            new ItemKey(ItemType.Object, "923"),
+            new ItemKey(ItemType.Object, "924"),
+            new ItemKey(ItemType.Object, "925"), // Slime Crate
+
+            // unobtainable
             new ItemKey(ItemType.Object, "30"), // Lumber
             new ItemKey(ItemType.Object, "94"), // Spirit Torch
             new ItemKey(ItemType.Object, "102"), // Lost Book
@@ -71,12 +155,7 @@ namespace ConvenientChests.CategorizeChests.Framework
             new ItemKey(ItemType.Object, "461"), // Decorative Pot
             new ItemKey(ItemType.Object, "528"), // Jukebox Ring
             new ItemKey(ItemType.Object, "590"), // Artifact Spot
-            new ItemKey(ItemType.Object, "742"), // Haley's Lost Bracelet
-            new ItemKey(ItemType.Object, "788"), // Lost Axe
-            new ItemKey(ItemType.Object, "789"), // Lucky Purple Shorts
-            new ItemKey(ItemType.Object, "790"), // Berry Basket
             new ItemKey(ItemType.Object, "892"), // Warp Totem: Qi's Arena
-            new ItemKey(ItemType.Object, "925"), // Slime Crate
             new ItemKey(ItemType.Object, "927"), // Camping Stove
             new ItemKey(ItemType.Object, "929"), // Hedge
             
